0b7dce71d384c2567cbf979c1bfcae70c7dfedab
Rob <Rob@BorBook.local>
2026-04-22 15:27:24 +0200

    Fix: AndroidApplicationConventionPlugin return type

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -4,20 +4,22 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
-    override fun apply(target: Project) = with(target) {
-        with(pluginManager) {
-            apply("com.android.application")
-            apply("org.jetbrains.kotlin.android")
-            apply("org.jetbrains.kotlin.plugin.compose")
-        }
-        configure<ApplicationExtension> {
-            configureAndroidCommon(this)
-            defaultConfig.targetSdk = 35
-            buildFeatures.compose = true
-            compileOptions {
-                isCoreLibraryDesugaringEnabled = true
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+                apply("org.jetbrains.kotlin.plugin.compose")
             }
+            configure<ApplicationExtension> {
+                configureAndroidCommon(this)
+                defaultConfig.targetSdk = 35
+                buildFeatures.compose = true
+                compileOptions {
+                    isCoreLibraryDesugaringEnabled = true
+                }
+            }
+            dependencies.add("coreLibraryDesugaring", "com.android.tools:desugar_jdk_libs:2.0.4")
         }
-        target.dependencies.add("coreLibraryDesugaring", "com.android.tools:desugar_jdk_libs:2.0.4")
     }
 }
