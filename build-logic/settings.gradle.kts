a25c46d9114538f1cc34e15e808bf58cb76b8309
libravault-xyz <rob@libravault.xyz>
2026-04-23 04:47:45 +0000

    fix: add mannodermaus plugin to build-logic pluginManagement
    The build-logic included build needs its own pluginManagement block
    to resolve plugins used by convention plugins.

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,3 +1,14 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("de.mannodermaus.android-junit5") version "1.10.0"
+    }
+}
+
 dependencyResolutionManagement {
     repositories {
         google()
