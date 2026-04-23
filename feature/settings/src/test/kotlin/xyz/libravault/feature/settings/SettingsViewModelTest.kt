5f48cead4981ff6719f2d5bef5a2856651906fbc
libravault-xyz <rob@libravault.xyz>
2026-04-23 09:14:53 +0000

    Fix: Import advanceUntilIdle in SettingsViewModelTest

--- feature/settings/src/test/kotlin/xyz/libravault/feature/settings/SettingsViewModelTest.kt
+++ feature/settings/src/test/kotlin/xyz/libravault/feature/settings/SettingsViewModelTest.kt
@@ -5,6 +5,7 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
